b6007f7d182df3ee23b6b6146e0f7680c0bc1e0b
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2025-12-28 01:12:05 +0000

    hurr

--- .gitlab/ci/build.sh
+++ .gitlab/ci/build.sh
@@ -225,7 +225,7 @@ _requirements() {
   _libevm_release_latest="0.0.0.0.0.0.0.0.1.1.1.1.1.1.1-4"
   _node_run_release_latest="0.0.0.0.0.0.0.0.1-8"
   _python_aiohttp_retry_release_latest="2.8.3-4"
-  _python_aioetherscan_release_latest="0.9.6.1-3"
+  _python_aioetherscan_release_latest="0.9.6.1-7"
   _python_asyncio_throttle_release_latest="1.0.2-4"
   _gur_mini \
     "${ns}" \
